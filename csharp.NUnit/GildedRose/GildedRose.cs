ffc0487cf68ebe2a4dc3087274180e6ecf5fafad
Maxime Mohandi <mohandi.maxime@gmail.com>
2025-11-28 11:07:20 +0100

    refactor:  simplify agedBrie quality computation

--- csharp.NUnit/GildedRose/GildedRose.cs
+++ csharp.NUnit/GildedRose/GildedRose.cs
@@ -19,17 +19,11 @@ public class GildedRose
             {
                 case SpecialItem.AgedBrie:
                 {
-                    if (item.Quality < 50)
-                    {
-                        item.Quality += 1;
-
-                    }
-
                     item.SellIn -= 1;
 
-                    if (item.SellIn < 0 && item.Quality < 50)
+                    if (item.Quality < 50)
                     {
-                        item.Quality += 1;
+                        item.Quality += item.SellIn < 0 ? 2 : 1;
                     }
 
                     break;
