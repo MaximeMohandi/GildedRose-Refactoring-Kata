e9599a77dc79acb5259ce38a8be3c89a61e481e5
Maxime Mohandi <mohandi.maxime@gmail.com>
2025-11-28 14:04:13 +0100

    refactor:  move computing in own method

--- csharp.NUnit/GildedRose/GildedRose.cs
+++ csharp.NUnit/GildedRose/GildedRose.cs
@@ -1,8 +1,9 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
 
 namespace GildedRoseKata;
 
-public class GildedRose
+public partial class GildedRose
 {
     private readonly IList<Item> _store;
 
@@ -11,82 +12,84 @@ public class GildedRose
         _store = store;
     }
 
-
-
     public void UpdateQuality()
     {
         foreach (var item in _store)
         {
-            switch (item.Name)
+            ComputeItemQuality(item);
+        }
+    }
+
+    private static void ComputeItemQuality(Item item)
+    {
+        switch (item.Name)
+        {
+            case SpecialItem.AgedBrie:
             {
-                case SpecialItem.AgedBrie:
+                item.DecreaseSellIn();
+
+                if (item.IsAtMaxQuality()) break;
+
+                if (item.IsSellPassed())
                 {
-                    item.DecreaseSellIn();
+                    item.IncreaseQualityTwice();
+                }
+                else
+                {
+                    item.IncreaseQuality();
+                }
 
-                    if (item.IsAtMaxQuality()) continue;
+                break;
+            }
+            case SpecialItem.BackstagePasses:
+            {
+                if (!item.IsAtMaxQuality())
+                {
+                    item.IncreaseQuality();
 
-                    if (item.IsSellPassed())
-                    {
-                        item.IncreaseQualityTwice();
-                    }
-                    else
+
+                    if (item.SellIn < 11 && !item.IsAtMaxQuality())
                     {
                         item.IncreaseQuality();
                     }
-
-                    break;
-                }
-                case SpecialItem.BackstagePasses:
-                {
-                    if (item.Quality < 50)
-                    {
-                        item.IncreaseQuality();
-
-
-                        if (item.SellIn < 11 && item.Quality < 50)
-                        {
-                            item.IncreaseQuality();
-                        }
-                        if (item.SellIn < 6 && item.Quality < 50)
-                        {
-                            item.IncreaseQuality();
-                        }
-                    }
-
-
-                    item.DecreaseSellIn();
-
-                    if (item.SellIn >= 0) continue;
-                    item.Quality -= item.Quality;
-                    break;
-                }
-                case SpecialItem.Sulfuras:
-                {
-                    if (item.Quality < 50)
+                    if (item.SellIn < 6 && !item.IsAtMaxQuality())
                     {
                         item.IncreaseQuality();
                     }
-                    break;
                 }
-                default:
+
+                item.DecreaseSellIn();
+
+                if (!item.IsSellPassed()) return;
+                item.Quality -= item.Quality;
+                break;
+            }
+            case SpecialItem.Sulfuras:
+            {
+                if (item.IsAtMaxQuality()) return;
+
+                item.IncreaseQuality();
+
+                break;
+            }
+            default:
+            {
+                switch (item.Quality)
                 {
-                    switch (item.Quality)
-                    {
-                        case > 0:
-                            item.DecreaseQuality();
-                            break;
-                        case < 50:
-                            item.IncreaseQuality();
-                            break;
-                    }
-
-                    item.SellIn -= 1;
-                    if (item.SellIn >= 0) continue;
-
-                    if (item.Quality <= 0) continue;
-                    item.DecreaseQuality();
-                    break;
+                    case > 0:
+                        item.DecreaseQuality();
+                        break;
+                    case < 50:
+                        item.IncreaseQuality();
+                        break;
                 }
+
+                item.DecreaseSellIn();
+                if (!item.IsSellPassed()) return;
+
+                if (item.Quality <= 0) return;
+                item.DecreaseQuality();
+                break;
             }
         }
     }
