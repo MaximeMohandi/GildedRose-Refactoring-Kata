f15791c33fdd986783fe5088d698694edc014c6a
Maxime Mohandi <mohandi.maxime@gmail.com>
2025-11-28 10:42:33 +0100

    refactor:  isolate backstages pass

--- csharp.NUnit/GildedRose/GildedRose.cs
+++ csharp.NUnit/GildedRose/GildedRose.cs
@@ -30,6 +30,29 @@ public class GildedRose
                     item.Quality += 1;
                 }
             }
+            else if (item.Name == SpecialItem.BackstagePasses)
+            {
+                if (item.Quality < 50)
+                {
+                    item.Quality += 1;
+
+                    if (item.SellIn < 11 && item.Quality < 50)
+                    {
+                        item.Quality += 1;
+                    }
+
+                    if (item.SellIn < 6 && item.Quality < 50)
+                    {
+                        item.Quality += 1;
+                    }
+                }
+
+
+                item.SellIn -= 1;
+
+                if (item.SellIn >= 0) continue;
+                item.Quality -= item.Quality;
+            }
             else
             {
                 if (item.Name == SpecialItem.BackstagePasses
@@ -100,6 +123,7 @@ public class GildedRose
                     }
                 }
             }
+            }
         }
     }
-}
+
